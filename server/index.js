b8e811bddf36def8d20f14cab035cc25e7054b21
謝佳穎 <chiayinghsieh212@gmail.com>
2023-01-11 15:05:22 +0800

    yarn install update

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
 let REDIRECT_URI = process.env.REDIRECT_URI || 'http://localhost:8000/callback';
 let FRONTEND_URI = process.env.FRONTEND_URI || 'http://localhost:3000';
-const PORT = process.env.PORT || 8888;
+const PORT = process.env.PORT || 8000;
 
 if (process.env.NODE_ENV !== 'production') {
   REDIRECT_URI = 'http://localhost:8000/callback';
