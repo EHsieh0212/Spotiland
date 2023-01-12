85bea7ee29c5a7ced6dd6ad78d2a151dcf4450b8
謝佳穎 <chiayinghsieh212@gmail.com>
2023-01-12 20:44:36 +0800

    adsad

--- client/src/spotify/crawl.js
+++ client/src/spotify/crawl.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const predict = async (name, length, seeding, textSeed) => {
-    const url = "http://localhost:4004/predict";
+    const url = "http://54.254.122.144/predict";
     const data = { name: name, length: length, seeding: seeding, textSeed: textSeed };
     return axios({
         method: 'post', url, data, headers: {
